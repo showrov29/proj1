ada7052eab66ea967f99fcc7c9b912257efb2915
Atif Ahmed <showrovislam29@gmail.com>
2023-02-24 00:43:08 +0600

    laravel practice 1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Login</title>
+</head>
+<body>
+
+ <Form>
+    <label for="login" class="control-label" > Username</label>
+        <input type="text" class="form-control" placeholder=" Enter username " name="uName" >
+        <br>
+        <br>
+    
+    <label for="login" class="control-label" > Password</label>
+        <input type="password" class="form-control" placeholder=" Enter Password " name="pass" >
+    <br>
+    <input type="submit" class="btn btn-primary value" value="Login" >
+ </Form>
+
+    
+</body>
+</html>
